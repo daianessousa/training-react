c68ee93fbacaf806af80505edeb9af834202deb9
daianessousa <daisousa.c@gmail.com>
2021-06-17 20:48:58 -0300

    api-config

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,28 @@
+import './styles.css';
+import { Component } from 'react';
+import { loadPosts } from '../../arqvs/load-posts';
+import { Posts } from '../../components/Posts';
+
+export class Home extends Component {
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
+    );
+  }
+}
